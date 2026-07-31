895dbee3c0aa4557f9a136b3dd56a4108007df56
Sivasurya-joker <ssivasurya211@gmail.com>
2026-07-31 15:36:00 +0530

    collazt.c

--- 09.Loops/collazt.c
+++ 09.Loops/collazt.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
-void collazt(int n){
-    if (n=n%2==0)
-    {
-        n=n/2;
-    }
-    else
-    {
-      
-    }
+int main (){
+    int n;
+    printf("enter the number ;");
+    scanf("%d",&n);
+
+        while (n!=1)
+        {
+            if (n%2==0)
+            {
+                n=n/2;
+            }
+            else
+            {
+                n=n*3+1;
+            } 
+            if(n%2==0)
+            {
+            printf("your number is :%d\n Its is even \n ",n);
+            }
+            else 
+            {
+             printf("your number is :%d\n Its is odd \n ",n);
+            }
+        }
+
+ return 0;
 }
