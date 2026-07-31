1efef054aaa89ea06d51e5e0a0fff917a4497f10
Sivasurya-joker <ssivasurya211@gmail.com>
2026-07-31 12:24:25 +0530

    update in code

---
+++ 09.Loops/collazt.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void collazt(int n){
+    if (n=n%2==0)
+    {
+        n=n/2;
+    }
+    else
+    {
+      
+    }
+}
--- 09.Loops/gameguess.c
+++ 09.Loops/gameguess.c
@@ -8,10 +8,10 @@ int main ()
   srand((time (NULL)));
   int value = rand ()%100+1;
   int n;
-  
+  int loop =5;
   int i = 0;
             
-  while (1)
+  for (int i=0; i<loop;i++)
   {
 
       printf("Choose the number form [1-100]=");
@@ -21,21 +21,22 @@ int main ()
     {
      printf("Your guess is Rigth !!! \n");
      printf("Youse win the game !!! \n");
+     break;
     }
     else if (n<value)
     {
       printf("Your guess is lower, Try Again! \n");
     }
-    else if(n>value)
+    else 
     {
       printf("Your guess is higher,Take another chance Again\n");
     }
-    else
+    if (i==loop-1)
     {
-      printf("You lose the game ,Thankyou for palying the Game \n");
+      printf("You lose the game\n  Thankyou for palying the Game \n");
       printf("THe number is :%d\n",value);
+      break;
     }
-
   }
-return 0;
+   return 0;
 }
