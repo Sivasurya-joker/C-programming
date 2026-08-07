1061ca8f9da7daa83c0d4b25f8e6333383a3abda
Sivasurya-joker <ssivasurya211@gmail.com>
2026-08-07 19:07:41 +0530

    error fixed

--- 09.Loops/reverse.c
+++ 09.Loops/reverse.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
+int get_num(){
+   int n;
+   printf("Enter the number :");
+   scanf("%d",&n);
+   return n;//get fuction and return nmuber itself
+}
+void print_reverse_num(int n)
+{
+
+
+  for (int i=n;i>0;i--){
+     printf("%d\n",i); 
+  }
+} 
 int main (){
-   int i ;
-     for (i=10;i>=0;i--){
-        printf("%d\n",i); 
-     }
+   int n =get_num();//assing n to get input.
+
+   
+   print_reverse_num(n);
+
   return 0;
 }
