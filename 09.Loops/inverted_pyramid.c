b9f8553d3b2466dae9bea9c287c4d6ae1b34311a
Sivasurya-joker <ssivasurya211@gmail.com>
2026-08-07 13:09:15 +0530

    modification in file

--- 09.Loops/inverted_pyramid.c
+++ 09.Loops/inverted_pyramid.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
-void invertpyramid(){
 
-    
-    int n, i, j, s;
+int get_input_form_user()
+{
+    int n;
+
     printf("Enter the number of rows: ");
     scanf("%d", &n);
-    for (i = n; i >= 1; --i) {
-       for (s = 0; s< n - i; ++s){
-           printf("  ");
-       }
-       for (j = i; j <= 2 * i - 1; ++j){
-           printf("* ");
-       }
-       for (j = 0; j < i - 1; ++j){ 
-           printf("*");
-       }
-    
-       printf("\n");
-    }
-    
+
+    return n;
 }
-int main() {
-   invertpyramid();
-   return 0;
+
+void print_inverted_pyramid(int n)
+{
+    int i, j, s;
+
+    for (i = n; i >= 1; i--)
+    {
+        // Print spaces
+        for (s = 0; s < n - i; s++)
+        {
+            printf("  ");
+        }
+
+        // Print left half of stars
+        for (j = i; j <= 2 * i - 1; j++)
+        {
+            printf("* ");
+        }
+
+        // Print right half of stars
+        for (j = 0; j < i - 1; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n;
+
+    n = get_input_form_user();
+
+    print_inverted_pyramid(n);
+
+    return 0;
 }
