1853e6e3ce92ece00d4ac1c86c6d40c2588198ac
Sivasurya-joker <ssivasurya211@gmail.com>
2026-08-07 18:39:30 +0530

    modify the code

--- 09.Loops/random_generator_10.c
+++ 09.Loops/random_generator_10.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main (){
+
+  void get_the_number()
+  {
     int n ;
     printf("Enter the limit:");
     scanf("%d",&n);
-
+  }
+  void generate4_the_random_number ()
+  {
+    int n;
     for(int i=0;i<n;i++)
     {
     int value =rand();
     printf("%d\n",value);
+    break;
     }
+  }
+int main (){
+  get_the_number();
+  generate4_the_random_number();
   return 0;
 }
