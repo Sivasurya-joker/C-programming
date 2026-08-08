d92642ce03330dc0e78314787ff5d561e47ecc5f
Sivasurya-joker <ssivasurya211@gmail.com>
2026-08-08 17:01:49 +0530

    function created

--- 09.Loops/numbers.c
+++ 09.Loops/numbers.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
+int  get_input()
+{
+int a;
+printf("enter the limit:");
+scanf("%d",&a);
+return a;
+}
+void print_numbers(int a){
+int i, j, k;
+for (i = 0; i <= a; i++) { /* rows */
+   for (j = 0; j <= a-i; j++) {
+      printf("   ");
+   }
+   /* stars */
+   for (k = 0; k <= 2*i-1; k++) {
+      printf("  %d",k);
+   }
+   printf("\n");
+}
+}
 int main()
 {
    int a;
-   printf("enter the limit:");
-   scanf("%d",&a);
-
-   int i, j, k;
-   for (i = 0; i <= a; i++) { /* rows */
-      for (j = 0; j <= a-i; j++) {
-         printf("   ");
-      }
-      /* stars */
-      for (k = 0; k <= 2*i-1; k++) {
-         printf("  %d",k);
-      }
-      printf("\n");
-   }
+   a = get_input();
+  print_numbers(a);
 
    return 0;
 }
