5f8f99bc897020b025f7dd3fdeae6e5a7a171ecc
Sivasurya-joker <ssivasurya211@gmail.com>
2026-08-29 18:00:58 +0530

    changes in the code of flames

--- 15.GameConsole/flames.c
+++ 15.GameConsole/flames.c
@@ -10,17 +10,6 @@ void get_username(char name[][100], int n)
     }
 }
 
-int count_remaining(char name[][100])
-{
-    int count = 0;
-
-    for (int i = 0; i < 2; i++)
-    {
-        count += strlen(name[i]);
-    }
-
-    return count;
-}
 
 void compare_and_remove_common(char name[][100])
 {
@@ -31,11 +20,11 @@ void compare_and_remove_common(char name[][100])
             if (name[0][i] == name[1][j])//check the user name are even or not.
             {
                 for (int k = i; name[0][k] != '\0'; k++)//check the commom letter and remove them in user 1.
-                    name[0][k] = name[0][k + 1];
-
+                name[0][k] = name[0][k + 1];
+                
                 for (int k = j; name[1][k] != '\0'; k++)// check the common letter and remove them in user 2.
-                    name[1][k] = name[1][k + 1];
-
+                name[1][k] = name[1][k + 1];
+                
                 i--;//decrease the length of username,
                 break;
             }
@@ -63,43 +52,49 @@ void flames(int count)
         case 'F':
             printf("Friends\n");
             break;
-
-        case 'L':
+            
+            case 'L':
             printf("Love\n");
             break;
-
-        case 'A':
+            
+            case 'A':
             printf("Affection\n");
             break;
-
-        case 'M':
+            
+            case 'M':
             printf("Marriage\n");
             break;
-
-        case 'E':
+            
+            case 'E':
             printf("Enemies\n");
             break;
-
-        case 'S':
+            
+            case 'S':
             printf("Siblings\n");
             break;
+        }
     }
-}
+    
+    int count_remaining(char name[][100])
+    {
+        int count = 0;
+    
+        for (int i = 0; i < 2; i++)
+        {
+            count += strlen(name[i]);
+        }
+    
+        return count;
+    }
 
-int main()
+    int main()
 {
     char name[2][100];
     int count;
-
     get_username(name, 2);
-
     compare_and_remove_common(name);
-
     count = count_remaining(name);
-
     printf("Remaining letters: %d\n", count);
-
     flames(count);
-
     return 0;
 }
