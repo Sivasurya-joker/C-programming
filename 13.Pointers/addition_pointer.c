b25af8ab1f7727aacd8b774e45604b20425ce30d
Sivasurya-joker <ssivasurya211@gmail.com>
2026-09-01 17:37:56 +0530

    add add_pointer.c

--- 13.Pointers/addition_pointer.c
+++ 13.Pointers/addition_pointer.c
@@ -1,17 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int get_the_number(int *a, int *b)
+{
+    printf("Enter the value of a :");
+    scanf("%d", &*a);
 
-    int n;
-    
+    printf("Enter the value of b :");
+    scanf("%d", &*b);
 
+    return 0;
+}
 
+void sum(int *a, int *b, int *add)
+{
+    *add = *a + *b;
 
+}
 
+int main()
+{
+    int a;
+    int b;
+    int add;
 
+    get_the_number(&a, &b);     
 
+    sum(&a, &b, &add);          
 
+    printf("Sum = %d\n", add);
 
-
+    return 0;
 }
