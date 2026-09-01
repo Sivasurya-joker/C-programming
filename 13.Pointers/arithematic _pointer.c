f9df6e647685bc77a3e825fc61750cbbf14ec4f9
Sivasurya-joker <ssivasurya211@gmail.com>
2026-09-01 16:07:37 +0530

    update changes

--- 13.Pointers/arithematic _pointer.c
+++ 13.Pointers/arithematic _pointer.c
@@ -1,27 +1,36 @@
-#include<stdio.h>
-#include<stdlib.h>
-void get_the_number(int n) 
+#include <stdio.h>
+#include <stdlib.h>
+
+void get_the_number(int *n)
 {
-printf("enter the number for arithematic operation in pointer :");
-scanf("%d",&n);
+    printf("Enter the number for arithmetic operation in pointer:\n");
+    scanf("%d", n);
 }
-void print_the_value(int n ,int *arithmatic_operations)
+
+void print_the_value(int *n, int **arithmatic_operations)
 {
+    printf("n = %d\n", *n);
 
-printf("arithematic_operations = %u\n",arithmatic_operations);
-arithmatic_operations++;
+    printf("arithmatic_operations = %p\n",
+           (void *)*arithmatic_operations);
 
-printf("arithematic_operations++= %u\n",arithmatic_operations);
-arithmatic_operations--;
+    (*arithmatic_operations)++;
 
+    printf("arithmatic_operations++ = %p\n",
+           (void *)*arithmatic_operations);
 
+    (*arithmatic_operations)--;
 }
-int main (){
-    int n ;
-    int *arithmatic_operations=&n;
-   
-    get_the_number(n);
-    print_the_value(n,arithmatic_operations);
+
+int main()
+{
+    int n;
+
+    int *arithmatic_operations = &n;
+
+    get_the_number(&n);
+
+    print_the_value(&n, &arithmatic_operations);
 
     return 0;
 }
