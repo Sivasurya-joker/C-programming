24f2ea4040090cf461b0e743ae6ea5daca09c827
Sivasurya-joker <ssivasurya211@gmail.com>
2026-09-02 11:57:44 +0530

    changes in code

--- 13.Pointers/sub_pointer.c
+++ 13.Pointers/sub_pointer.c
@@ -1,1 +1,29 @@
 #include<stdio.h>
+int main () {
+    
+    int buy ;
+    printf("Enter the cost price :");
+    scanf("%d",&buy);
+    
+    int sell;
+    printf("Enter the sell price :");
+    scanf("%d",&sell);
+    
+    
+    
+    int *cp=&buy;
+    int *sp=&sell;
+     
+   printf("cp=%u\nsp=%u\n", *cp, *sp);
+
+    int discount =buy-sell;
+
+    printf("\nDiscount of sp""&cp is %d\n",discount);
+
+    
+     printf("Your Business will face a loss area.\n");
+    
+
+    return 0;
+
+}
